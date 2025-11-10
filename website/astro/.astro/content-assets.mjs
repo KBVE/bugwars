0190bd7153be446749279d5f407108829213d48a
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-11-10 14:32:49 -0500

    feat: astro pages template

---
+++ website/astro/.astro/content-assets.mjs
@@ -0,0 +1,8 @@
+
+import __ASTRO_IMAGE_IMPORT_1MpTut from "../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Faskama.mdx";
+import __ASTRO_IMAGE_IMPORT_Z2gGaEW from "../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_ZuNf2r from "../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fja%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_1jw60y from "../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fes%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_ZRMgER from "../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fko%2Findex.mdx";
+export default new Map([["../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Faskama.mdx", __ASTRO_IMAGE_IMPORT_1MpTut], ["../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z2gGaEW], ["../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fja%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZuNf2r], ["../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fes%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1jw60y], ["../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fko%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZRMgER]]);
+		
---
+++ website/astro/.astro/content-modules.mjs
@@ -0,0 +1,8 @@
+
+export default new Map([
+["src/content/docs/askama.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Faskama.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/ja/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/es/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fes%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/ko/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fko%2Findex.mdx&astroContentModuleFlag=true")]]);
+		
