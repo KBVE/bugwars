646effd7c2a80e2486ac54de7852725daf8fdbf5
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-11-12 12:55:52 -0500

    feat: minor tweaks to the gdd.

--- website/astro/.astro/content-modules.mjs
+++ website/astro/.astro/content-modules.mjs
@@ -1,14 +1,14 @@
 
 export default new Map([
-["src/content/docs/game.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgame.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/askama.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Faskama.mdx&astroContentModuleFlag=true")],
+["src/content/docs/game.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgame.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/gdd.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgdd.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/blog/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fblog%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/es/game.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fes%2Fgame.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/es/gdd.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fes%2Fgdd.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/es/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fes%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/ja/game.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Fgame.mdx&astroContentModuleFlag=true")],
-["src/content/docs/blog/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fblog%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/ja/gdd.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Fgdd.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/ja/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/ko/game.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fko%2Fgame.mdx&astroContentModuleFlag=true")],
